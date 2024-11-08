cd877c7cb620d2fcb6c311d972d3562ed8625363
Akila-Nipo <akila.nipo1104@gmail.com>
2024-11-08 20:17:01 +0600

    added CI

--- SmartClassRoutineManagementSystem/config/db.js
+++ SmartClassRoutineManagementSystem/config/db.js
@@ -1,20 +1,26 @@
-//config/db.js
-
 const mysql = require('mysql');
 
-const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "db_class_routine"
-});
+// If the environment is CI, mock the database connection (skip real connection)
+if (process.env.CI === 'true') {
+    console.log('Skipping actual DB connection in CI environment');
+    module.exports = {
+        query: () => { return { yields: () => {} }; }, // Mock query method
+    };
+} else {
+    const db = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "db_class_routine"
+    });
 
-db.connect((err) => {
-    if (err) {
-        console.error('Error connecting to MySQL:', err);
-        throw err;
-    }
-    console.log('Connected to MySQL database');
-});
+    db.connect((err) => {
+        if (err) {
+            console.error('Error connecting to MySQL:', err);
+            throw err;
+        }
+        console.log('Connected to MySQL database');
+    });
 
-module.exports = db;
+    module.exports = db;
+}
